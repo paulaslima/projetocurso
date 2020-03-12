4384384f9b58e87175bf2fb70d8f9559558de5de
paulaslima <61985251+paulaslima@users.noreply.github.com>
2020-03-12 13:45:51 -0300

    Initial commit

---
+++ app/Http/Controllers/TesteController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TesteController extends Controller
+{
+    public function index() {
+      return view ('Biblioteca/index');
+
+    }
+
+}
